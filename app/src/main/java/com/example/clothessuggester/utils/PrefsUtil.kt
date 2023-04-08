76e3ede0548b8e4718f26d2f46892d5d2160279b
أســـــاور <xyzas.7510@gmail.com>
2023-04-08 15:50:29 +0300

    add prefs unit

--- app/src/main/java/com/example/clothessuggester/utils/PrefsUtil.kt
+++ app/src/main/java/com/example/clothessuggester/utils/PrefsUtil.kt
@@ -6,13 +6,13 @@ import android.content.SharedPreferences
 object PrefsUtil {
     private var sharedPreferences: SharedPreferences? = null
     private const val SHARED_PREF = "ClothPrefs"
-    private val KEY = ""
+    private val KEY = "clothes"
 
     fun initPrefUtil(context: Context){
         sharedPreferences = context.getSharedPreferences(SHARED_PREF,Context.MODE_PRIVATE)
 
     }
-    var userName: String?
+    var clothName: String?
         get() = sharedPreferences?.getString(KEY,null)
         set(value) {
             sharedPreferences?.edit()?.putString(KEY,value)?.apply()
